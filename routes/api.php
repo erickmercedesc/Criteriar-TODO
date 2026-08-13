7faae0e82e8abb510bc816cbe54c5b2d9c773fca
Erick Mercedes <erickmercedesemc@gmail.com>
2026-08-13 19:02:10 -0400

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:api')->group(function () {
+Route::middleware('auth:api')->name('api.')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
